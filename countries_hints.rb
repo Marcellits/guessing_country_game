f9cab9475eacd25e33c3858a78d65a24def57bd5
marcellits <garcia.marcella@hotmail.com>
2020-08-25 17:48:03 -0400

    Module Countries Hints added

---
+++ countries_hints.rb
@@ -0,0 +1,57 @@
+module CountriesHints
+    COUNTRIES_HINTS = {
+        FRANCE: [
+            "Its Gastronomy is very famous worldwide.",
+            "Historically, they don't like England.",
+            "Its capital is the city of love.",
+        ],
+        GERMANY: [
+            "Its national flag is black, red and yellow.",
+            "Its president is a woman.",
+            "This country is famous for love beer."
+    
+        ],
+        CANADA:[
+            "It was one of the British colonies.",
+            "Poutine is the most famous dish.",
+            "This country has two oficial languages: French and English."
+        
+        ],
+        JAPAN:[
+            "Its national flag is white and red.",
+            "Anime is very famous in this country.",
+            "Their food is well known worldwide."
+        ],
+        BRAZIL: [
+            "Its national flag is green, yellow, blue, and white.",
+            "Natives speak Portuguese.",
+            "This country is famous for its national soccer team."
+            
+        ],
+        PERU: [
+            "This country is located in South America.",
+            "The capital is Lima.",
+            "One of the world wonders is in this country."
+        ],
+        ARGENTINA: [
+            "The Pope was born there.",
+            "This country is famous for Tango.",
+            "Lionel Messi is one of their favorite idols."
+        ],
+        INDIA: [
+            "The industry of cinema, called Bollywood, is very strong.",
+            "This country is located in Asia.",
+            "Mahatma Ghandi was born there."
+        ],
+        EGYPT:[
+            "One of the oldest civilization of the World.",
+            "One of the Seven Wonders of the World is in there.",
+            "Antropologists and tourists love this country",
+        ],
+        MADAGASCAR:[
+            "It is an island country in Africa.",
+            "The capital is Antananarivo.",
+            "This country became famous for being the namesake of a popular animated film."
+        ]
+    }
+end
